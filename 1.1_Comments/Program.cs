86453e87b4b29d8948cb09ff2d4a4d5d0c3867e2
chriswainscott <chrisdwianscott@gmail.com>
2016-08-01 19:07:07 -0400

    deleted 3 duplicate files

--- 1.1_Comments/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _1._1_Comments
-{
-    //This is a good way to do one line of code but not effecient for a paragraph
-
-    /* if you have several lines of code or a paragraph that you need 
-     * to code out then you should use this method to 
-     * commetn your code */
-
-}
