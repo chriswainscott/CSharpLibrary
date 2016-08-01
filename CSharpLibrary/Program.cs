87e64fdea04cb03433b7385e18df4d6661f4f03d
chriswainscott <chrisdwianscott@gmail.com>
2016-08-01 17:44:19 -0400

    New Project

--- CSharpLibrary/Program.cs
+++ CSharpLibrary/Program.cs
@@ -10,6 +10,8 @@ namespace CSharpLibrary
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Console"); //#1 This will print to the console.
+            Console.ReadLine(); //#2 This will make the console open.
         }
     }
 }
